bc23fa586a28fafc1bc84250a3c3fe101646eb24
Jose <jcdeveloper7@gmail.com>
2021-07-16 12:01:21 -0700

    added connection to mongoose and cors

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express                   = require('express');
+const mongoose                  = require('mongoose');
 const morgan                    = require('morgan');
 
 const app                       = express();
@@ -6,7 +7,28 @@ const port                      = 4000;
 
 require('dotenv').config();
 
+mongoose.connect(process.env.MONGODATABASE, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true});
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to databse: ', process.env.MONGODATABASE)
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log('Database error: ', err);
+});
+
+// this is a logger
 app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Expose-Headers', 'Authorization, refresh');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, refresh');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+  
+    next();
+});
 
 
 app.listen(port, () => { console.log(`Listening on port ${port}`)});
