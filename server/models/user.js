bef368daecf62faafc98285b787b0b10f9249d35
Jose <jcdeveloper7@gmail.com>
2021-07-19 06:23:03 -0700

    feat: created UserSchema

---
+++ server/models/user.js
@@ -0,0 +1,14 @@
+const mongoose            = require("mongoose"),
+
+const UserSchema = new mongoose.Schema({
+    username: { type: String, required: true, index: { unique: true } },
+    email: { type: String, required: true, index: { unique: true } },
+    password: { type: String, required: true },
+    isAdmin: Boolean,
+    token: String
+}, {
+    timestamps: true,
+    collection: 'Users'
+});
+
+module.exports = mongoose.model('User', UserSchema);
